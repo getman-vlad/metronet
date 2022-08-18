e3bb37cd02b12e9e35ad284c87e446d78291094b
metronet <none>
2022-08-18 01:34:10 -0500

    renamed:    Dockerfile -> metronetapp/Dockerfile 	renamed:    website/oldlondon.ttf -> metronetapp/website/fonts/oldlondon.ttf 	renamed:    website/royalcrescent.otf -> metronetapp/website/fonts/royalcrescent.otf 	renamed:    website/index.html -> metronetapp/website/index.html 	renamed:    website/script.js -> metronetapp/website/script.js 	renamed:    website/style.css -> metronetapp/website/style.css 	modified:   restart.sh

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 docker stop metronet
 docker rm metronet
 docker rmi  metronet:$CV
-docker build -t metronet:$CV .
+docker build -t metronet:$CV metronetapp/
 docker run -d \
 --name metronet \
 -p 80:80 \
